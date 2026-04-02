2990e86540b73bf7d0bb39f8d58816ffa2aa61e4
Advaittej Prattipati <143482989+AdvaittejPS@users.noreply.github.com>
2026-04-02 23:57:33 +0530

    Update df_multiplier_c3_tb.v

--- src/testbenches/df_multiplier_c3_tb.v
+++ src/testbenches/df_multiplier_c3_tb.v
@@ -2,39 +2,55 @@
 
 module df_multiplier_c3_tb;
 	
-	integer i;
+	integer j;
+	integer exact_val, approx_val, error_dist;
+	integer total_error = 0;
 	
 	reg [7:0] data;
 	wire [7:0] out;
 	
 	wire [7:0] c;
-	wire [15:0] mul;
+	wire [15:0] mul_exact;
+	wire [7:0] out_exact;
 	
+	// Instantiate the Approximate Multiplier
 	df_multiplier_c3 dut(data, out);
 	
+	// Calculate exact expected value for baseline comparison
 	assign c = 8'b00011011;
-	
-	assign mul = data * c;
+	assign mul_exact = data * c;
+	assign out_exact = mul_exact[15:8];
 	
 	initial begin
 		$dumpfile("df_multiplier_c3_tb.vcd");
 		$dumpvars;
-		for (i = 0; i <= 3; i = i + 1) begin
-			$dumpvars(0, dut.stage1[i]);
-		end
-		for (i = 0; i <= 2; i = i + 1) begin
-			$dumpvars(0, dut.stage2[i]);
-		end
-		for (i = 0; i <= 1; i = i + 1) begin
-			$dumpvars(0, dut.stage3[i]);
-		end
 		
-		data = 8'h00;
+		$display("Running Approximate Multiplier C3 Test...");
+		$display("Data | Exact Out | Approx Out | Error");
+		$display("------------------------------------------");
 		
-		for (i = 0; i <= 255; i = i + 51) begin
-			data = i;
+		for (j = 0; j <= 255; j = j + 1) begin
+			data = j[7:0];
 			#5;
+			
+			exact_val = out_exact;
+			approx_val = out;
+			
+			// Calculate absolute error distance
+			if (exact_val > approx_val)
+				error_dist = exact_val - approx_val;
+			else
+				error_dist = approx_val - exact_val;
+				
+			total_error = total_error + error_dist;
+			
+			// Print errors
+			if (error_dist > 0 && j % 16 == 0)
+				$display(" %3d |    %3d    |     %3d    |   %d", data, exact_val, approx_val, error_dist);
 		end
-		#5 $finish;
+		
+		$display("------------------------------------------");
+		$display("Mean Error Distance (MED) for Multiplier C3: %f", total_error / 256.0);
+		$finish;
 	end
 endmodule
